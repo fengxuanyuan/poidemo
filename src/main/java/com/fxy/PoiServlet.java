b96140bd93f7129e42b8bfb863a0b0e138fd312b
fengxuanyuan2010@foxmail.com <fengxuanyuan2010@foxmail.com>
2020-04-22 09:01:24 +0800

    初始化

--- src/main/java/com/fxy/PoiServlet.java
+++ src/main/java/com/fxy/PoiServlet.java
@@ -8,6 +8,7 @@ package com.fxy;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
@@ -18,19 +19,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import static org.apache.poi.ss.usermodel.CellType.BLANK;
-import static org.apache.poi.ss.usermodel.CellType.STRING;
+import static org.apache.poi.ss.usermodel.CellType.*;
 
 public class PoiServlet extends HttpServlet {
 
@@ -40,65 +39,66 @@ public class PoiServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            doPost(request,response);
+        doPost(request, response);
 
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = req.getParameter("_m");
-        if("poi_down".equals(method)){
-            poi_down(req,resp);
-        }else if("poi_upload".equals(method)){
-            poi_upload(req,resp);
+        if ("poi_down".equals(method)) {
+            poi_down(req, resp);
+        } else if ("poi_upload".equals(method)) {
+            poi_upload(req, resp);
         }
     }
 
 
     private void poi_upload(HttpServletRequest request,
                             HttpServletResponse response) {
-        if(ServletFileUpload.isMultipartContent(request)){
+        if (ServletFileUpload.isMultipartContent(request)) {
             DiskFileItemFactory factory = new DiskFileItemFactory();
-            factory.setSizeThreshold(1024*512);
+            factory.setSizeThreshold(1024 * 512);
             factory.setRepository(new File("D:/tempload"));
-            ServletFileUpload fileUpload=new ServletFileUpload(factory);
-            fileUpload.setFileSizeMax(10*1024*1024);//设置最大文件大小
+            ServletFileUpload fileUpload = new ServletFileUpload(factory);
+            fileUpload.setFileSizeMax(10 * 1024 * 1024);//设置最大文件大小
             try {
                 @SuppressWarnings("unchecked")
-                List<FileItem> items=fileUpload.parseRequest(request);//获取所有表单
-                for(FileItem item:items){
+                List<FileItem> items = fileUpload.parseRequest(request);//获取所有表单
+                for (FileItem item : items) {
                     //判断当前的表单控件是否是一个普通控件
-                    if(!item.isFormField()){
+                    if (!item.isFormField()) {
                         //是一个文件控件时
                         String excelFileName = new String(item.getName().getBytes(), "utf-8"); //获取上传文件的名称
                         //上传文件必须为excel类型,根据后缀判断(xls)
                         String excelContentType = excelFileName.substring(excelFileName.lastIndexOf(".")); //获取上传文件的类型
-                        System.out.println("上传文件名:"+excelFileName);
-                        System.out.println("文件大小:"+item.getSize());
+                        System.out.println("上传文件名:" + excelFileName);
+                        System.out.println("文件大小:" + item.getSize());
                         System.out.println("\n---------------------------------------");
-                        if(".xlsx".equalsIgnoreCase(excelContentType)){
-                           // POIFSFileSystem fileSystem = new XSSFWorkbook(item.getInputStream());
+                        if (".xlsx".equalsIgnoreCase(excelContentType)) {
+                            // POIFSFileSystem fileSystem = new XSSFWorkbook(item.getInputStream());
                             XSSFWorkbook workbook = new XSSFWorkbook(item.getInputStream());
                             XSSFSheet sheet = workbook.getSheetAt(0);
                             int rows = sheet.getPhysicalNumberOfRows();
                             for (int i = 2; i < rows; i++) {
                                 XSSFRow row = sheet.getRow(i);
-
-                                    int columns = row.getPhysicalNumberOfCells();
-
-                                    for (int j = 0; j < columns; j++) {
-                                        XSSFCell cell = row.getCell(j);
-                                        if(cell != null && cell.getCellType() != BLANK) {
-                                            cell.setCellType(STRING);
-                                            String value = this.getCellStringValue(cell);
-                                            System.out.print(value + "|");
-                                        }
+                                if (filterBlankRow(i, row)) {
+                                    continue;
+                                }
+                                int columns = row.getPhysicalNumberOfCells();
+                                for (int j = 0; j < columns; j++) {
+                                    XSSFCell cell = row.getCell(j);
+                                    if (cell != null && cell.getCellType() != BLANK) {
+                                        cell.setCellType(STRING);
+                                        String value = this.getCellStringValue(cell);
+                                        System.out.print(value + "|");
                                     }
+                                }
 
                                 System.out.println("\n---------------------------------------");
                             }
                             System.out.println("success！");
-                        }else if(".xls".equalsIgnoreCase(excelContentType)){
+                        } else if (".xls".equalsIgnoreCase(excelContentType)) {
                             POIFSFileSystem fileSystem = new POIFSFileSystem(item.getInputStream());
                             HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
                             HSSFSheet sheet = workbook.getSheetAt(0);
@@ -106,18 +106,20 @@ public class PoiServlet extends HttpServlet {
                             int rows = sheet.getPhysicalNumberOfRows();
                             for (int i = 0; i < rows; i++) {
                                 HSSFRow row = sheet.getRow(i);
-                                if(row.getFirstCellNum()>-1) {
-                                    int columns = row.getPhysicalNumberOfCells();
-                                    for (int j = 0; j < columns; j++) {
-                                        HSSFCell cell = row.getCell(j);
-                                        String value = this.getCellStringValue(cell);
-                                        System.out.print(value + "|");
-                                    }
+                                if (filterBlankRow(i, row)) {
+                                    continue;
                                 }
+                                int columns = row.getPhysicalNumberOfCells();
+                                for (int j = 0; j < columns; j++) {
+                                    HSSFCell cell = row.getCell(j);
+                                    String value = this.getCellStringValue(cell);
+                                    System.out.print(value + "|");
+                                }
+
                                 System.out.println("\n---------------------------------------");
                             }
                             System.out.println("success！");
-                        }else{
+                        } else {
                             System.out.println("必须为excel类型");
                         }
                         //顺便把文件保存到硬盘,防止重名
@@ -130,10 +132,29 @@ public class PoiServlet extends HttpServlet {
                         response.sendRedirect("index.jsp");
                     }
                 }
-            }catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * @param i
+     * @param row
+     * @return
+     * @Description :过滤存在空行
+     * @Author fengxuanyuan2010@foxmail.com
+     * @Date 2020/4/22 08:28
+     */
+    private boolean filterBlankRow(int i, Row row) {
+        if (i >= 1) {
+            if (row == null) {
+                return true;
+            } else if (StringUtils.isEmpty(getValue(row.getCell(0))) && StringUtils.isEmpty(getValue(row.getCell(1)))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void poi_down(HttpServletRequest request,
@@ -165,10 +186,10 @@ public class PoiServlet extends HttpServlet {
         cell2.setCellStyle(style);
 
         //创建数据行
-        for(int i =1;i<=20;i++) {
+        for (int i = 1; i <= 20; i++) {
             HSSFRow newrow = sheet.createRow(i);
-            newrow.createCell(0).setCellValue("tom"+i);
-            newrow.createCell(1).setCellValue("135816****"+i);
+            newrow.createCell(0).setCellValue("tom" + i);
+            newrow.createCell(1).setCellValue("135816****" + i);
         }
         try {
             workbook.write(stream);
@@ -181,6 +202,31 @@ public class PoiServlet extends HttpServlet {
 
     }
 
+    /**
+     * @param cell
+     * @return
+     */
+    private String getValue(Cell cell) {
+        if (cell == null) {
+            return "";
+        } else if (cell.getCellType() == BOOLEAN) {
+            return String.valueOf(cell.getBooleanCellValue());
+        } else if (cell.getCellType() == NUMERIC) {
+            String value = "";
+            //检验是否为日期格式的数值类型
+            if (DateUtil.isCellDateFormatted(cell)) {
+                value = new SimpleDateFormat().format(cell.getDateCellValue());
+            } else {
+                value = new DecimalFormat("0").format(cell.getNumericCellValue());
+            }
+            return value;
+        } else {
+            return String.valueOf(cell.getStringCellValue());
+        }
+
+    }
+
+
     //获取单元格内不同类型值
     public String getCellStringValue(Cell cell) {
         try {
@@ -188,8 +234,8 @@ public class PoiServlet extends HttpServlet {
             switch (cell.getCellType()) {
                 case STRING:
                     cellValue = cell.getStringCellValue();
-                    if(cellValue.trim().equals("")||cellValue.trim().length()<=0)
-                        cellValue=" ";
+                    if (cellValue.trim().equals("") || cellValue.trim().length() <= 0)
+                        cellValue = " ";
                     break;
                 case NUMERIC:
                     cellValue = String.valueOf(cell.getNumericCellValue());
@@ -199,7 +245,7 @@ public class PoiServlet extends HttpServlet {
                     cellValue = String.valueOf(cell.getNumericCellValue());
                     break;
                 case BLANK:
-                    cellValue=" ";
+                    cellValue = " ";
                     break;
                 case BOOLEAN:
                     break;
